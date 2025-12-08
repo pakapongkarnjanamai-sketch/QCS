1eacece0d7d2cf02bc55f7a18e522ef9171dadeb
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2025-12-08 08:24:54 +0700

    StatusConsts

---
+++ QCS.Domain/Enum/StatusConsts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QCS.Domain.Enum
+{
+    public static class StatusConsts
+    {
+        public const string Pending = "Pending";
+        public const string Approved = "Approved";
+        public const string Rejected = "Rejected";
+        public const string Completed = "Completed";
+    }
+}
