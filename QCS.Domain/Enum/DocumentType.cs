845db21fe699792e8f1828724e49b9a9977c3480
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2025-12-09 15:42:58 +0700

    Rename PurchaseRequest

---
+++ QCS.Domain/Enum/DocumentType.cs
@@ -0,0 +1,10 @@
+﻿namespace QCS.Domain.Enum
+{
+    public enum DocumentType
+    {
+        MainQuotation = 10,
+        ComparativeQuotation = 20,
+        Specifications = 30,      
+        TermsAndConditions = 40   
+    }
+}
