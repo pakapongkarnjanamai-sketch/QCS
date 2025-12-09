f64623052f95b2afd5c4e743bfcfd858957772ae
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2025-12-09 17:02:48 +0700

    add

---
+++ QCS.Web.User/Controllers/RequestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace QCS.Web.User.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly ILogger<RequestController> _logger;
+
+        public RequestController(ILogger<RequestController> logger)
+        {
+            _logger = logger;
+        }
+
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+   
+
+        [Route("Request/Form/{id?}")]
+        public IActionResult Form(int? id)
+        {
+            return View(id);
+        }
+    }
+}
