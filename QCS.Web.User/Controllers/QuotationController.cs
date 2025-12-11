7298fe496bb98d7d0fd7353d926289ec989897b4
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2025-12-11 17:06:02 +0700

    public

---
+++ QCS.Web.User/Controllers/QuotationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QCS.Web.User.Controllers
+{
+    public class QuotationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // === เพิ่มส่วนนี้ ===
+        public IActionResult Detail(string id)
+        {
+            // ส่ง id (ที่เป็น String Code) ไปให้หน้า View
+            return View("Detail", id);
+        }
+    }
+}
